0cdb77ade2b49a562350b3fccaae74d91b638502
TomoYan <tomoyan596@gmail.com>
2021-06-23 16:46:10 +0900

    Create hello.py
    My first binder.

---
+++ hello.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+ 
+def get_name():
+    return 'Binder'
+ 
+def main():
+    print(f'Hello from {get_name()}!')
+ 
+if __name__ == '__main__':
+    main()
